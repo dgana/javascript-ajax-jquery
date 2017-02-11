a752e862a4efe605f42bbc088cbe51244a20cfff
Ida Bagus Chahya Dhegana <dhegana@gmail.com>
2017-02-11 17:33:40 +0700

    done color change

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # javascript-ajax-jquery
+
 Change the color of the box after click event using jQuery AJAX!
+
+```
+npm i
+cd javascript-ajax-jquery/public -> live-server
+```
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/colors', function(req, res, next) {
+  let randomIndex = Math.floor(Math.random() * 9) // random number between 0 - 8
+  let randomFirstRGB = Math.floor(Math.random() * 255)
+  let randomSecondRGB = Math.floor(Math.random() * 255)
+  let randomThirdRGB = Math.floor(Math.random() * 255)
+  res.json({i: randomIndex, rgb1: randomFirstRGB, rgb2: randomSecondRGB, rgb3: randomThirdRGB})
 });
 
 module.exports = router;
